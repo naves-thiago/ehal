1c1b2481634948e357ef57761fa0a2da42b17980
Marcelo Politzer Couto <mpolitzer.c@gmail.com>
2010-12-28 17:01:19 -0200

    Adding callback prototype and fixing doxygen comments.

--- ehal/port.h
+++ ehal/port.h
@@ -1,10 +1,9 @@
 /** @file
  * @defgroup port port
  * @{
- * @example portpinex.c
- */
-#ifndef __PORT_H_
-#define __PORT_H_
+ * @example portpinex.c */
+#ifndef EHAL_PORT
+#define EHAL_PORT
 
 #include "types.h"
 #include "port_specific.h"
@@ -14,21 +13,19 @@
 
 /** Put the port in a know state.
  * Input, no pullup, no pulldown, no interrupt.
- * @arg id - port id.
+ * @param id - port id.
  * @note check if it is valid before using any other function with
- * @port_isvalid.
- */
+ * @port_isvalid. */
 void	port_init	(u08 id);
 
 /** Runtime validation of a port @a id. return 1 for a valid port.
- * @arg id - port id.
- */
+ * @param id - port id. */
 u08	port_isvalid	(u08 id);
 
 /** Set (HIGH/LOW)
- * @arg id - port id.
- * @arg mask - masks the bits that will be changed.
- * @arg val - the new values for the masked bits. */
+ * @param id - port id.
+ * @param mask - masks the bits that will be changed.
+ * @param val - the new values for the masked bits. */
 void	port_write	(u08 id, port_t mask, port_t val);
 
 /** Read the port value, all its pins.
@@ -36,35 +33,38 @@ void	port_write	(u08 id, port_t mask, port_t val);
  * undefined. */
 port_t	port_read	(u08 id);
 
+/** Change the port direction (IN/OUT).
+ * @param id - port id.
+ * @param mask - masks the bits that will be changed.
+ * @param val - the new values for the masked bits. 1 - in, 0 - out. */
+void	port_setdir	(u08 id, port_t mask, port_t val);
 
-/** Set pullup, the arguments work like the port_write function.
- * @arg id - port id.
- * @arg mask - masks the bits that will be changed.
- * @arg val - the new values for the masked bits. */
+/** Get the port direction.
+ * @param id - port id. */
+port_t	port_getdir	(u08 id);
+
+/** Set pullup, the paramuments work like the port_write function.
+ * @param id - port id.
+ * @param mask - masks the bits that will be changed.
+ * @param val - the new values for the masked bits. */
 void	port_setpullup	(u08 id, port_t mask, port_t val);
 
 /** Read the current the pullups.
- * @arg id - port id. */
+ * @param id - port id. */
 port_t	port_getpullup	(u08 id);
 
-/** Set pull down, the arguments work like the port_write function.
- * @arg id - port id.
- * @arg mask - masks the bits that will be changed.
- * @arg val - the new values for the masked bits. */
+/** Set pull down, the paramuments work like the port_write function.
+ * @param id - port id.
+ * @param mask - masks the bits that will be changed.
+ * @param val - the new values for the masked bits. */
 void	port_setpulldn	(u08 id, port_t mask, port_t val);
 
 /** Read the current position of the pullups. */
 port_t	port_getpulldn	(u08 id);
 
-/** Change the port direction (IN/OUT).
- * @arg id - port id.
- * @arg mask - masks the bits that will be changed.
- * @arg val - the new values for the masked bits. 1 - in, 0 - out. */
-void	port_setdir	(u08 id, port_t mask, port_t val);
-
-/** Get the port direction.
- * @arg id - port id. */
-port_t	port_getdir	(u08 id);
+/* TODO complete desciption. */
+/* interrupt "callback" */
+void	port_onchange	(u08 id, void (*callback)(void), port_t mask);
 
 #endif /* __PORT_H_ */
 
