d188c7907bf2dbbf8f3103942de577b839a25923
Marcelo Politzer Couto <mpolitzer.c@gmail.com>
2010-12-29 20:47:13 -0200

    uart with queue interface.

--- ehal/avr/uart_x.c
+++ ehal/avr/uart_x.c
@@ -1,16 +1,17 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include "cpu.h"
 #include "uart.h"
 #include "queue.h"
 
-#define UART_NUM 1
+#define UART_NUM	1
 
 struct uart {
 //	struct rwl rwl;
-	struct queue tx,rx;
+	struct queue *tx,*rx;
 };
 
-struct uart uart[UART_NUM];
+static struct uart uart[UART_NUM];
 
 
 u08 uart_isvalid (u08 id)
@@ -21,7 +22,7 @@ u08 uart_isvalid (u08 id)
 void uart_init (u08 id)
 {
 	UCSRC = 3<<UCSZ0;
-	UCSRA = (1<<U2X);
+	UCSRA |= (1<<U2X);
 	UCSRB = (1<<RXEN)
 		| (1<<TXEN)
 		| (1<<TXCIE)
@@ -30,80 +31,80 @@ void uart_init (u08 id)
 	sei ();
 }
 
-void uart_inittx (u08 id, u08 *buff, u08 sz)
+void uart_settxqueue (u08 id, struct queue *q)
 {
-	queue_init (&uart[id].tx, buff, sz);
+	uart[id].tx = q;
 }
 
-void uart_initrx (u08 id, u08 *buff, u08 sz)
+struct queue *uart_gettxqueue (u08 id)
 {
-	queue_init (&uart[id].rx, buff, sz);
+	return uart[id].tx;
+}
+
+void uart_setrxqueue (u08 id, struct queue *q)
+{
+	uart[id].rx = q;
+}
+
+struct queue *uart_getrxqueue (u08 id)
+{
+	return uart[id].rx;
 }
 
 #define ENTRY(_baud, _fcpu) B ## _baud:		\
 	UBRRH = (_fcpu/(8UL*_baud) - 1) >> 8;	\
 	UBRRL = (_fcpu/(8UL*_baud) - 1);
-
 void uart_setbaud (u08 id, u08 baud)
 {
 	switch(baud){
-	case ENTRY(   300, FCPU); break;
-	case ENTRY(  1200, FCPU); break;
-	case ENTRY(  2400, FCPU); break;
-	case ENTRY(  4800, FCPU); break;
+	case ENTRY(   300, CPU_FREQ); break;
+	case ENTRY(  1200, CPU_FREQ); break;
+	case ENTRY(  2400, CPU_FREQ); break;
+	case ENTRY(  4800, CPU_FREQ); break;
 	default:
-	case ENTRY(  9600, FCPU); break;
-	case ENTRY( 19200, FCPU); break;
-	case ENTRY(115200, FCPU); break;
+	case ENTRY(  9600, CPU_FREQ); break;
+	case ENTRY( 19200, CPU_FREQ); break;
+	case ENTRY(115200, CPU_FREQ); break;
 	}
 }
 #undef ENTRY
 
 u32 uart_getbaud (u08 id)
 {
-	return FCPU/(( UBRRL + (UBRRH << 8) + 1 )*8);
+	return CPU_FREQ/((UBRRL+(UBRRH << 8)+1)*8);
 }
 
 u08 uart_txready (u08 id)
 {
-	return queue_empty (&uart[id].tx)
-	       	&& (UCSRA & (1<<UDRE));
+	return UCSRA & _BV(UDRE);
 }
 
-u08 uart_write (u08 id, void *buff, u08 sz)
+/* The noinline attribute prevents a bug where uart_transmit is
+ * optimized to executed only once before the while loop.
+ *
+ * while (1){
+ *	uart_transmit (0);
+ * }
+ *
+ */
+void uart_transmit (u08 id) __attribute__((noinline));
+void uart_transmit (u08 id)
 {
-	u08 i;
-	if (sz == 0) return 0;
-	if (uart_txready (id)) {
-		UDR = *((u08 *)buff);
-		buff++;
-		sz--;
-	}
-	for (i=0; i<sz; i++)
-		if (queue_enq (&uart[id].tx, ((char *)buff) [i]))
-			break;
-	return i;
-}
-
-u08 uart_read (u08 id, void *buff, u08 sz)
-{
-	u08 i;
-	if (sz == 0) return 0;
-	for (i=0; i<sz; i++)
-		if (queue_deq (&uart[id].rx, &((u08 *)buff) [i]))
-			break;
-	return i;
+	unsigned char c;
+	if (!uart_txready (id)) return;
+	if (!queue_deq (uart[0].tx, &c))
+		UDR = c;
 }
 
 ISR(USART_RXC_vect)
 {
 	unsigned char c = UDR;
-	queue_enq (&uart[0].rx, c);
+	queue_enq (uart[0].rx, c);
 }
 
 ISR(USART_TXC_vect)
 {
 	unsigned char c;
-	if (queue_deq (&uart[0].tx, &c)) return;
-	UDR = c;
+	if (!queue_deq (uart[0].tx, &c))
+		UDR = c;
 }
