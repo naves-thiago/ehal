fe10a2c053945deddd84fa703df72eb1e46844b7
Marcelo Politzer Couto <mpolitzer.c@gmail.com>
2010-12-29 20:46:48 -0200

    queue updated with peak functions.

--- ehal/queue.c
+++ ehal/queue.c
@@ -15,33 +15,33 @@ unsigned char queue_ocupied (struct queue *q)
 	return q->ocupied;
 }
 
-unsigned char queue_full (struct queue *q)
+unsigned char queue_isfull (struct queue *q)
 {
 	return q->ocupied == q->sz;
 }
 
-unsigned char queue_empty (struct queue *q)
+unsigned char queue_isempty (struct queue *q)
 {
 	return q->ocupied == 0;
 }
 
 unsigned char queue_peakfirst (struct queue *q, unsigned char *ret)
 {
-	if (queue_empty( q )) return 1;
+	if (queue_isempty( q )) return 1;
 	*ret = q->buff[ q->s ];
 	return 0;
 }
 
 unsigned char queue_peaklast (struct queue *q, unsigned char *ret)
 {
-	if (queue_empty( q )) return 1;
+	if (queue_isempty( q )) return 1;
 	*ret = q->buff[ (q->s + q->ocupied-1) % q->sz ];
 	return 0;
 }
 
 unsigned char queue_enq (struct queue *q, unsigned char data)
 {
-	if (queue_full( q )) return 1;
+	if (queue_isfull( q )) return 1;
 	q->buff[ (q->s + q->ocupied) % q->sz ] = data;
 	q->ocupied++;
 	return 0;
@@ -49,7 +49,7 @@ unsigned char queue_enq (struct queue *q, unsigned char data)
 
 unsigned char queue_deq (struct queue *q, unsigned char *ret)
 {
-	if (queue_empty( q )) return 1;
+	if (queue_isempty( q )) return 1;
 	*ret = q->buff[ q->s++ ];
 	q->ocupied--;
 	if (q->s >= q->sz) q->s = 0;
--- ehal/queue.h
+++ ehal/queue.h
@@ -1,49 +1,50 @@
 /** @file Queue module of unsigned chars. */
-#ifndef _QUEUE_H_
-#define _QUEUE_H_
+#ifndef EHAL_QUEUE
+#define EHAL_QUEUE
 
 struct queue {
-	/** your buffer goes here. */
-	unsigned char *buff;
-	/** number of elements in buff */
-	unsigned char sz;
-	/** start, index of first element */
-	unsigned char s;
-	/** number of ocupied bytes */
-	unsigned char ocupied;
+	unsigned char *buff;	/**< your buffer goes here. */
+	unsigned char sz;	/**< number of elements in buff */
+	unsigned char s;	/**< start, index of first element */
+	unsigned char ocupied;	/**< number of ocupied bytes */
 };
 
-/** @brief set user defined buffer to be the queue buffer. */
+/** set user defined buffer to be the queue buffer. */
 void
 queue_init	(struct queue *q,
 		unsigned char *buff,
 		unsigned char sz);
 
-/** @brief returns how many bytes are in the queue. */
+/** returns how many bytes are in the queue. */
 unsigned char
 queue_ocupied	(struct queue *q);
 
-/** @brief returns non-zero if queue is full */
+/** returns non-zero if queue is full */
 unsigned char
-queue_full	(struct queue *q);
+queue_isfull	(struct queue *q);
 
-/** @brief returns non-zero if queue is empty */
+/** returns non-zero if queue is empty */
 unsigned char
-queue_empty	(struct queue *q);
+queue_isempty	(struct queue *q);
 
-/** @brief put data in the queue, return zero when success */
+/** put data in the queue, return zero when success */
 unsigned char
 queue_enq	(struct queue *q,
 		unsigned char data);
 
-/** @brief cp first element in the queue to *ret and remove it from queue */
+/** cp first element in the queue to *ret and remove it from queue */
 unsigned char
 queue_deq	(struct queue *q,
 		unsigned char *ret);
 
-/** @brief same as before but without removing it */
+/** same as before but without removing it */
 unsigned char
-queue_peek	(struct queue *q,
+queue_peaklast	(struct queue *q,
+		unsigned char *ret);
+
+/** same as before but for the first element */
+unsigned char
+queue_peakfirst	(struct queue *q,
 		unsigned char *ret);
 
 #endif		/* #ifndef __QUEUE_H_ */
