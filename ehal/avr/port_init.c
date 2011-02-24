dfb8077f089fb6bc3f793f15a9d0a6c6463d925d
Marcelo Politzer Couto <mpolitzer.c@gmail.com>
2011-02-24 05:11:45 -0300

    Using -D$(MCU) instead of __AVR_ ... to make sure it will get uniform throught the diferent platforms.

--- ehal/avr/port_init.c
+++ ehal/avr/port_init.c
@@ -5,44 +5,45 @@
 #define BIND_INDEX_WITH_PORT(BLOCK_START)\
 	(struct port_mem_block *)&PIN ## BLOCK_START
 
-#if defined (__AVR_ATtiny25__)	\
-	|| (__AVR_ATtiny45__)	\
-	|| (__AVR_ATtiny85__)
+#if defined (ehal_attiny25)	\
+	|| (ehal_attiny45)	\
+	|| (ehal_attiny85)
 struct port_mem_block *port_mem_block[] = {
 	BIND_INDEX_WITH_PORT (B),
 };
-#elif defined (__AVR_ATmega8__)	\
-	|| defined (__AVR_ATmega48__)	\
-	|| defined (__AVR_ATmega88__)	\
-	|| defined (__AVR_ATmega168__)
+#elif defined (ehal_atmega8)	\
+	|| defined (ehal_atmega48)	\
+	|| defined (ehal_atmega88)	\
+	|| defined (ehal_atmega168)	\
+	|| defined (ehal_atmega328)
 struct port_mem_block *port_mem_block[] = {
 	BIND_INDEX_WITH_PORT (B),
 	BIND_INDEX_WITH_PORT (C),
 	BIND_INDEX_WITH_PORT (D),
 };
-#elif	defined (__AVR_ATmega168P__)
+#elif	defined (ehal_atmega168p)
 struct port_mem_block *port_mem_block[] = {
 	BIND_INDEX_WITH_PORT (B),
 	BIND_INDEX_WITH_PORT (C),
 	BIND_INDEX_WITH_PORT (D),
 };
 
-#elif defined (__AVR_ATmega16__)	\
-	|| defined (__AVR_ATmega32__)
+#elif defined (ehal_atmega16)	\
+	|| defined (ehal_atmega32)
 struct port_mem_block *port_mem_block[] = {
 	BIND_INDEX_WITH_PORT (A),
 	BIND_INDEX_WITH_PORT (B),
 	BIND_INDEX_WITH_PORT (C),
 	BIND_INDEX_WITH_PORT (D),
 };
-#elif defined (__AVR_ATmega164P__)
+#elif defined (ehal_atmega164p)
 struct port_mem_block *port_mem_block[] = {
 	BIND_INDEX_WITH_PORT (A),
 	BIND_INDEX_WITH_PORT (B),
 	BIND_INDEX_WITH_PORT (C),
 	BIND_INDEX_WITH_PORT (D),
 };
-#elif defined (__AVR_ATmega128__)
+#elif defined (ehal_atmega128)
 struct port_mem_block *port_mem_block[] = {
 	BIND_INDEX_WITH_PORT (A),
 	BIND_INDEX_WITH_PORT (B),
@@ -53,7 +54,7 @@ struct port_mem_block *port_mem_block[] = {
 	BIND_INDEX_WITH_PORT (G),
 };
 #else
-#error MCU not defined in ehal/avr/port.c
+#error MCU not defined in ehal/avr/port_init.c
 #endif
 
 #define port_mask(p, m, v) do { p = ((p) & ~(m)) | ((v) & (m)); } while(0)
