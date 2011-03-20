3c518b80a19c8547fe37ee2ececd46dc3aadfe0a
Marcelo Politzer Couto <mpolitzer.c@gmail.com>
2011-03-19 23:47:10 -0300

    defining NUM_PORTS on port_specific.h

--- ehal/avr/port_specific.h
+++ ehal/avr/port_specific.h
@@ -6,4 +6,28 @@
 
 typedef u08 port_t;
 
+#if defined (ehal_attiny25)	\
+	|| (ehal_attiny45)	\
+	|| (ehal_attiny85)
+#define NUM_PORTS	1
+#elif defined (ehal_atmega8)	\
+	|| defined (ehal_atmega48)	\
+	|| defined (ehal_atmega88)	\
+	|| defined (ehal_atmega168)	\
+	|| defined (ehal_atmega328)
+#define NUM_PORTS	3
+#elif	defined (ehal_atmega168p)
+#define NUM_PORTS	3
+#elif defined (ehal_atmega16)	\
+	|| defined (ehal_atmega32)
+#define NUM_PORTS	4
+#elif defined (ehal_atmega164p)
+#define NUM_PORTS	4
+#elif defined (ehal_atmega128)
+#define NUM_PORTS	7
+#else
+#error MCU not defined in ehal/avr/port_init.c
+#endif
+
+
 #endif
