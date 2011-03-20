465fc4c79d061820156ceb87ad404087ba3b0b1b
Marcelo Politzer Couto <mpolitzer.c@gmail.com>
2011-03-20 15:29:35 -0300

    +doc, +enforcing NUM_UARTS

--- ehal/uart.h
+++ ehal/uart.h
@@ -13,7 +13,15 @@
 #endif
 
 #include <stdio.h>
+#include "uart_specific.h"
 #include "tinytypes.h"
+
+#ifndef NUM_UARTS
+/** Maximum number of uarts. */
+#define NUM_UARTS <defined in uart_specific.h>
+#error Undefined number of uarts
+#endif
+
 
 /** Possible baud rates. */
 enum uart_baud_rate {
@@ -30,22 +38,28 @@ enum uart_stopbits {
 	STOP_BITS_1, STOP_BITS_1_5, STOP_BITS_2,
 };
 
+#ifndef NUM_UARTS
+/** Number of uarts present on platform */
+#define NUM_UARTS <defined in uart_specific.h>
+#error undefined NUM_UARTS
+#endif
+
 /** Get a hdl from an id.
  * Puts the uart into the default state: 9600 8N1
  *
- * @param id - index of uart from 0 to NUM_UARTS.
+ * @param id - index of uart from 0 to @ref NUM_UARTS.
  * @return uart handler.
  ******************************************************************************/
 void	*uart_init (int id);
 
 /** Set to one of the @ref uart_baud_rate values.
- * @param id - index of uart from 0 to NUM_UARTS.
+ * @param id - index of uart from 0 to @ref NUM_UARTS.
  * @param baud - one of @ref uart_baud_rate.
  ******************************************************************************/
 void	uart_setbaud (void *id, enum uart_baud_rate baud);
 
 /** Get the numerical with error value.
- * @param id - index of uart from 0 to NUM_UARTS.
+ * @param id - index of uart from 0 to @ref NUM_UARTS.
  * @return The baud rate in bps.
  * @note This value might be diferent from the one passed on setbaud, problems
  * might be related to a crystal not multiple of baud.
@@ -53,44 +67,44 @@ void	uart_setbaud (void *id, enum uart_baud_rate baud);
 u32	uart_getbaud (void *id);
 
 /** Set number of bits for tx and rx. The default is 8.
- * @param id - index of uart from 0 to NUM_UARTS.
+ * @param id - index of uart from 0 to @ref NUM_UARTS.
  * @param n - number of bits, normally this value goes from 4 to 8.
  * Check if it worked with @ref uart_getnbits.
  ******************************************************************************/
 void	uart_setnbits (void *id, u08 n);
 
 /** Gets the number of bits of the transmission.
- * @param id - index of uart from 0 to NUM_UARTS.
+ * @param id - index of uart from 0 to @ref NUM_UARTS.
  * @return number of bits.
  ******************************************************************************/
 u08	uart_getnbits (void *id);
 
 /** Set the number of stop bits.
- * @param id - index of uart from 0 to NUM_UARTS.
+ * @param id - index of uart from 0 to @ref NUM_UARTS.
  * @param stopbits - 1, 1.5 or 2. Defaults to 1.
  ******************************************************************************/
 void	uart_setstopbits(void *id, enum uart_stopbits stopbits);
 
 /** Get the number of stop bits.
- * @param id - index of uart from 0 to NUM_UARTS.
+ * @param id - index of uart from 0 to @ref NUM_UARTS.
  * @return 1, 1.5 or 2.
  ******************************************************************************/
 enum uart_stopbits uart_getstopbits (void *id);
 
 /** Set parity.
- * @param id - index of uart from 0 to NUM_UARTS.
+ * @param id - index of uart from 0 to @ref NUM_UARTS.
  * @param par - none, even or odd.
  ******************************************************************************/
 void	uart_setparity (void *id, enum uart_parity par);
 
 /** Get parity.
- * @param id - index of uart from 0 to NUM_UARTS.
+ * @param id - index of uart from 0 to @ref NUM_UARTS.
  * @return none, even or odd.
  ******************************************************************************/
 enum uart_parity uart_getparity (void *id);
 
 /** write an array into the uart.
- * @param id - index of uart from 0 to NUM_UARTS.
+ * @param id - index of uart from 0 to @ref NUM_UARTS.
  * @param ptr - Pointer with data.
  * @param sz - number of bytes from ptr to write.
  * @return Actual number of written bytes.
@@ -98,7 +112,7 @@ enum uart_parity uart_getparity (void *id);
 int	uart_write (void *id, unsigned char *ptr, int sz);
 
 /** Reads sz bytes into ptr.
- * @param id - index of uart from 0 to NUM_UARTS.
+ * @param id - index of uart from 0 to @ref NUM_UARTS.
  * @param ptr - Buffer to write data.
  * @param sz - number of bytes to read.
  * @return Number successfuly readed.
