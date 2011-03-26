2e8f0e61ea4a20d563b4c2160998d9152f045929
Marcelo Politzer Couto <mpolitzer.c@gmail.com>
2011-03-25 22:57:42 -0300

    Removed uart from port test.

--- unity_test/main.c
+++ unity_test/main.c
@@ -16,22 +16,13 @@ void hd44780_write (char *str)
 int main (void)
 {
 	char buff[] = "eHal!";
-	unsigned char c;
 	void *p0;
-	void *u0;
 
-	cpu_init ();
 	p0 = port_init (0);
-	u0 = uart_init (0);
-	cpu_gie ();
 
 	hd44780_init ();
 	hd44780_write (buff);
 
-	uart_write (u0, buff, sizeof (buff)-1);
 	while (1){
-		uart_read (u0, &c, 1);
-		hd44780_write_buff_char (c);
-		if (c == '\r') hd44780_write_buff ();
 	}
 }
