64fb85b497f4d1ce3d194d61cb8e356534460257
naves.thiago <naves.thiago@gmail.com>
2011-03-28 00:48:34 -0300

    Merged the doc for the port.h file

--- ehal/port.h
+++ ehal/port.h
@@ -1,7 +1,8 @@
 /** @file
  * @defgroup port port
  * @{
- * @example portpinex.c */
+ * @example port_ex.c
+ ******************************************************************************/
 #ifndef EHAL_PORT_H
 #define EHAL_PORT_H
 
@@ -15,22 +16,78 @@
 /** Mask with all pins. */
 #define ALL_PINS	((port_t)-1)
 
+#ifndef NUM_PORTS
+/** Maximum number of ports. */
+#define NUM_PORTS <defined in port_specific.h>
+#error Undefined Number of ports
+#endif
+
+/** Get a port pointer from an index and puts the port in the default eHAL state.
+ * That is: no pullup, no pulldown and as input.
+ * @param id - The number of the port to work with.
+ * @return The port pointer to be passed to the other functions or NULL.
+ * @note index goes from 0 to @ref NUM_PORTS.
+ * ****************************************************************************/
 void *port_init (unsigned int id);
 
-int port_get_id (void *port);
-
+/** Sets the (val & mask) bits, and clears the ((~val) & mask) bits. That is:
+ * Change the bits masked as 1 and ignore the ones masked at 0.
+ * @param port - port handler got form @ref port_init.
+ * @param mask - the bits to change.
+ * @param val - the new value of the masked bits
+ ******************************************************************************/
 void port_write (void *port, port_t mask, port_t val);
+
+/** Read the value of port. This function doesn't know about pins operating with
+ * secundary functions nor the ones operating as output, both cases gives an
+ * undefined behaviour, so be sure to mask only the input bits.
+ * @param port - port handler got form @ref port_init.
+ * @return the bits that the por read.
+ ******************************************************************************/
 port_t port_read (void *port);
 
-void port_setdir (void *port, port_t mask, port_t val);
+/** Set the direction of port. (1 - Input, 0 - Output). The mask
+ * works the same as in port_write.
+ * @param port - port handler got form @ref port_init.
+ * @param mask - the bits to change.
+ * @param dir - the new value of the masked bits
+ ******************************************************************************/
+void port_setdir (void *port, port_t mask, port_t dir);
+
+/** Get the pins direction for each bit. (1 - in, 0 - out)
+ * @param port - port handler got form @ref port_init.
+ ******************************************************************************/
 port_t port_getdir (void *port);
 
+/** Turn on the pullup on the port.
+ * @param port - port handler got form @ref port_init.
+ * @param mask - the bits to change.
+ * @param val - the new value of the masked bits
+ ******************************************************************************/
 void port_setpullup (void *port, port_t mask, port_t val);
+
+/** Get the pullup.
+ * @param port - port handler got form @ref port_init.
+ ******************************************************************************/
 port_t port_getpullup (void *port);
 
+/** Turn on the pulldown on the port.
+ * @param port - port handler got form @ref port_init.
+ * @param mask - the bits to change.
+ * @param val - the new value of the masked bits
+ ******************************************************************************/
 void port_setpulldown (void *port, port_t mask, port_t val);
+
+/** Get the pulldown.
+ * @param port - port handler got form @ref port_init.
+ ******************************************************************************/
 port_t port_getpulldown (void *port);
 
+/** Turn off the pulldown and the pullup on the port.
+ * @param port - port handler got form @ref port_init.
+ * @param mask - the bits to change.
+ * @param val - the new value of the masked bits ( 1 = change, 0 = do nothing )
+ ******************************************************************************/
 void port_settristate(void *port, port_t mask, port_t val);
 
 #endif /* EHAL_PORT_H */
