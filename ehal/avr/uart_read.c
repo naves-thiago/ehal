36363ab262c65617ef6d7639cb9193a89fe88753
Marcelo Politzer Couto <mpolitzer.c@gmail.com>
2011-03-29 05:33:22 -0300

    Added CPU & UART to 88, 168, 328 and also corrected a UART bug.

--- ehal/avr/uart_read.c
+++ ehal/avr/uart_read.c
@@ -2,16 +2,19 @@
 #include "cpu.h"
 #include "uart_internal.h"
 
-extern uint8_t uart_rx_buff;
+extern uint8_t uart_rx_buff[RX_BUFF_SZ];
 extern uint8_t uart_rx_head;
 extern uint8_t uart_rx_tail;
 
 int uart_read (void *unused, unsigned char *ptr, int sz)
 {
-	while (sz){
-		if (sz && (uart_rx_head != uart_rx_tail)){
-			sz--;
+	int readed=0;
+	while (readed < sz){
+		if (uart_rx_head != uart_rx_tail){
+			uart_rx_head = (uart_rx_head+1) & (RX_BUFF_SZ-1);
+			*ptr = uart_rx_buff[uart_rx_head];
 			ptr++;
+			readed++;
 		} else {
 			cpu_sleep (sleep_mode_iddle);
 		}
--- ehal/avr/uart_write.c
+++ ehal/avr/uart_write.c
@@ -8,29 +8,29 @@ extern char uart_tx_buff[TX_BUFF_SZ];
 extern uint8_t uart_tx_head;
 extern uint8_t uart_tx_tail;
 
-static inline bool tx_is_iddle (void)
+static inline bool tx_is_ready (void)
 {
 	return UCSR0A & _BV (UDRE0);
 }
 
 int uart_write (void *unused, char *ptr, int sz)
 {
+	int written;
 	if (sz == 0) return 0;
 
-	while (sz && uart_tx_head != uart_tx_tail){
+	for (written=0; written < sz; written++){
 		uart_tx_tail = (uart_tx_tail+1) & (RX_BUFF_SZ-1);
-		uart_tx_buff[uart_tx_tail] = *ptr;
-		ptr++;
-		sz--;
+		uart_tx_buff[uart_tx_tail] = *ptr++;
 	}
 
-	/* Send the first byte. */
-	uart_tx_tail = (uart_tx_tail+1) & (RX_BUFF_SZ-1);
-	UDR0 = uart_tx_buff[uart_tx_tail];
-
+	if (tx_is_ready ()){
+		/* Send the first byte. */
+		uart_tx_head = (uart_tx_head+1) & (RX_BUFF_SZ-1);
+		UDR0 = uart_tx_buff[uart_tx_head];
+	}
 	while (uart_tx_head != uart_tx_tail){
 		cpu_sleep (sleep_mode_iddle);
 	}
 
-	return sz;
+	return written;
 }
