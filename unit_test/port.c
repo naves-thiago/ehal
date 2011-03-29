5351c7a1626de68cf1412c1a867b25522f2e021d
Marcelo Politzer Couto <mpolitzer.c@gmail.com>
2011-03-29 05:55:50 -0300

    Simple example

--- unit_test/port.c
+++ unit_test/port.c
@@ -1,37 +1,17 @@
 /*******************************************************************************
  * @file
- * This is partial port test.
+ * This is a simple test, suposed to toogle leds or something like that.
  ******************************************************************************/
 
 #include "ehal/port.h"
-#define LONG_TIME 65534
 
 int main (void)
 {
-	unsigned int i;
-	port_t v;
 	void *p;
 
 	p = port_init (0);
 
-	/* Mirror the 0,1,2,3 bits into 4,5,6,7. */
-	i = LONG_TIME;
 	port_setdir (p, ALL_PINS, 0x0F);
-	while (i--){
-		v = port_read (p) & 0x0F;
-		port_write (p, ALL_PINS, v<<4);
-	}
-
-	/* Mirror the 0,1,2,3 bits into 4,5,6,7. With pullup enabled */
-	i = LONG_TIME;
-	port_setdir (p, ALL_PINS, 0x0F);
-	while (i--){
-		v = port_read (p) & 0x0F;
-		port_write (p, ALL_PINS, v<<4);
-	}
-
-	/* Toggle the port forever. */
-	port_setdir (p, ALL_PINS, 0);
 	while (1){
 		port_write (p, ALL_PINS, ALL_PINS);
 		port_write (p, ALL_PINS, 0);
